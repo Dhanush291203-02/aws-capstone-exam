8697c7c9dc57deb449609350f4d9951d208d0f75
Dhanush291203-02 <dhanushg2912@gmail.com>
2026-02-06 04:55:16 +0000

    Create index.php

---
+++ app/v1/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$server_ip = $_SERVER['SERVER_ADDR'];
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Streamline App - v1</title>
+
+</head>
+
+<body style="background-color: #f2f2f2;">
+
+    <h1>Welcome to Streamline - v1</h1>
+
+    <p>Server IP: <?php echo $server_ip; ?></p>
+
+</body>
+
+</html>
